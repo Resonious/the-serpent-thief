f4dcc6483dd2e4a6ee6704caad45c44e31cd6cb8
Nigel Baillie <metreckk@gmail.com>
2014-12-25 20:15:11 -0500

    Tag dropdown links now link properly

--- app/helpers/stories_helper.rb
+++ app/helpers/stories_helper.rb
@@ -20,4 +20,26 @@ module StoriesHelper
       end
     end
   end
+
+  def clean_story_path(story, tag = nil)
+    story = story.find_by(link: story) unless story.is_a?(Story)
+    tag = case tag
+          when Tag then tag.value
+          else tag
+          end
+
+    if story.active?
+      if tag
+        read_story_path(tag)
+      else
+        root_path
+      end
+    else
+      if tag
+        read_story_tag_path(story.link, tag)
+      else
+        read_story_path(story.link)
+      end
+    end
+  end
 end
