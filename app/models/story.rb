73a74d91fc19e88d2664fb941d1e30362432a5fe
Nigel Baillie <metreckk@gmail.com>
2015-02-01 16:39:15 -0500

    Story tags should be uniq

--- app/models/story.rb
+++ app/models/story.rb
@@ -1,6 +1,6 @@
 class Story < ActiveRecord::Base
   has_many :pages, dependent: :destroy, inverse_of: :story
-  has_many :tags, through: :pages
+  has_many :tags, -> { uniq }, through: :pages
   validates_presence_of :name
   validates_uniqueness_of :name
 
