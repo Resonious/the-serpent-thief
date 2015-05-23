be5aa2b54550b127c16154b5c36b398a0d9d8c85
Nigel Baillie <metreckk@gmail.com>
2015-05-23 14:24:28 -0400

    more ui overhaul

--- app/assets/javascripts/global.js
+++ app/assets/javascripts/global.js
@@ -9,7 +9,42 @@ function unsavedChangesToPage(condition) {
   return 'You have unsaved changes to the current page. Are you sure?';
 }
 
+function setLogoWidth() {
+  if ($('#page-box').length == 0) {
+    var pageWidth = $(window).width();
+    var logoWidth = $('img.logo').data('width');
+
+    var newWidth  = Math.min(pageWidth * 0.8, logoWidth);
+    var newHeight = newWidth * (859 / 3108);
+
+    $('.logo').width(newWidth);
+    $('.logo').height(newHeight);
+  } else {
+    var newWidth  = $('#page-box').width() * (3108 / 2600);
+    var newHeight = newWidth * (859 / 3108);
+    $('.logo').width(newWidth);
+    $('.logo').height(newHeight);
+
+    var pos   = $('.logo').offset();
+    var pageX = $('#page-box').offset().left;
+
+    var offsetToBox = newWidth * (175 / 3108);
+
+    pos.left = pageX - offsetToBox;
+
+    $('.logo').offset(pos);
+  }
+}
+
 $(function() {
+  $(window).resize(setLogoWidth);
+  setLogoWidth();
+
+  $('#homepage-link').click(function() {
+    window.location = $('#homepage-link > a').href();
+    return false;
+  });
+
   if ($('#active-warning').length) {
     $('#active-warning').hide();
 
@@ -25,11 +60,4 @@ $(function() {
       });
     }
   }
-
-  $('.tag-drop-hover').mouseover(function() {
-    var storyTags = $(this).next('.story-tags');
-    var dropdown = storyTags.find('.tags-dropdown');
-
-    dropdown.dropdown('toggle');
-  });
 });
